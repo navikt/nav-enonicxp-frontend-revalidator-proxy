ca730c9395e7ee88319bd91aaed4afab4b4dd0c5
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-03-01 12:47:44 +0100

    Diverse refactor

--- src/app.js
+++ src/app.js
@@ -14,12 +14,10 @@ const clientsAddressHeartbeat = {};
 
 const { SERVICE_SECRET } = process.env;
 
-const options = { headers: { secret: SERVICE_SECRET } };
-
 const jsonBodyParser = bodyParser.json();
 
 // Revalidate requests from Enonic XP are sent independently by every node in the server cluster.
-// Each request has an associated eventId, which corresponds to a publishing event. This id is identical across all
+// Each request has an associated eventid, which corresponds to a publishing event. This id is identical across all
 // servers per event.
 //
 // This object keeps track of which requests per event that have already been proxied to the frontend and prevents
@@ -27,32 +25,45 @@ const jsonBodyParser = bodyParser.json();
 const recentEvents = {
     eventTimeout: 10000,
     eventStatus: {},
-    updateEventStatus: function (eventId) {
-        if (!eventId) {
+    updateEventStatus: function (eventid) {
+        if (!eventid) {
             return false;
         }
 
-        if (this.eventStatus[eventId]) {
+        if (this.eventStatus[eventid]) {
             return true;
         }
 
-        console.log(`Adding entry for event ${eventId}`);
-        this.eventStatus[eventId] = true;
+        console.log(`Adding entry for event ${eventid}`);
+        this.eventStatus[eventid] = true;
 
         setTimeout(() => {
-            console.log(`Event ${eventId} expired`);
-            delete this.eventStatus[eventId];
+            console.log(`Event ${eventid} expired`);
+            delete this.eventStatus[eventid];
         }, this.eventTimeout);
 
         return false;
     },
 };
 
-const callClients = (callback) => {
+const callClients = (path, eventid, options) => {
     Object.entries(clientsAddressHeartbeat).forEach(
         ([address, lastHeartbeat]) => {
             if (Date.now() - lastHeartbeat < clientStaleTime) {
-                callback(address);
+                const url = `http://${address}:${clientPort}${path}`;
+                fetch(url, options)
+                    .then((res) => {
+                        if (!res.ok) {
+                            throw new Error(
+                                `${res.status} - ${res.statusText}`
+                            );
+                        }
+                    })
+                    .catch((e) =>
+                        console.error(
+                            `Request to ${url} failed for event ${eventid} - ${e}`
+                        )
+                    );
             } else {
                 console.log(`Removing stale client: ${address}`);
                 delete clientsAddressHeartbeat[address];
@@ -62,70 +73,69 @@ const callClients = (callback) => {
 };
 
 app.post('/revalidator-proxy', jsonBodyParser, (req, res) => {
-    const { secret, eventId } = req.headers;
+    const { secret, eventid } = req.headers;
 
     if (secret !== SERVICE_SECRET) {
-        console.error(`Proxy request denied for event ${eventId} (401)`);
+        console.error(`Proxy request denied for event ${eventid} (401)`);
         return res.status(401).send('Not authorized');
     }
 
     const { paths } = req.body;
 
     if (!Array.isArray(paths)) {
+        console.error(`Bad request for event ${eventid}`);
         return res
             .status(400)
             .send('Body field "paths" is required and must be an array');
     }
 
-    const eventWasProcessed = recentEvents.updateEventStatus(eventId);
+    const eventWasProcessed = recentEvents.updateEventStatus(eventid);
 
     if (eventWasProcessed) {
-        const msg = `Event ${eventId} has already been processsed`;
+        const msg = `Event ${eventid} has already been processsed`;
         console.log(msg);
         return res.status(200).send(msg);
     }
 
-    callClients((address) =>
-        fetch(`http://${address}:${clientPort}?invalidatePaths=true`, {
-            ...options,
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ paths, eventId }),
-        }).catch((e) =>
-            console.error(
-                `Error while requesting revalidation to ${address} of ${eventId} - ${e}`
-            )
-        )
-    );
+    callClients('/invalidate', eventid, {
+        method: 'POST',
+        headers: {
+            secret: SERVICE_SECRET,
+            eventid,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ paths }),
+    });
 
-    const msg = `Sent invalidation request for eventId ${eventId} to all clients - Paths: ${paths.join(
+    const msg = `Sent invalidation request for event ${eventid} to all clients - Paths: ${paths.join(
         ', '
     )}`;
     console.log(msg);
+
     res.status(200).send(msg);
 });
 
 app.get('/revalidator-proxy/wipe-all', (req, res) => {
-    const { secret } = req.headers;
+    const { secret, eventid } = req.headers;
 
     if (secret !== SERVICE_SECRET) {
-        console.error(`Proxy request denied for wipe-all (401)`);
+        console.error(
+            `Proxy request denied for wipe-all event id ${eventid} (401)`
+        );
         return res.status(401).send('Not authorized');
     }
 
-    callClients((address) =>
-        fetch(`http://${address}:${clientPort}?wipeAll=true`, options).catch(
-            (e) =>
-                console.error(
-                    `Error while requesting revalidation to ${address} of ${encodedPath} - ${e}`
-                )
-        )
-    );
+    callClients('/invalidate/wipe-all', eventid, {
+        method: 'GET',
+        headers: {
+            secret: SERVICE_SECRET,
+            eventid,
+        },
+    });
 
     const msg = 'Sent wipe-all request to all clients';
     console.log(msg);
+
     res.status(200).send(msg);
 });
 
@@ -166,7 +176,6 @@ const server = app.listen(appPort, () => {
         throw new Error(msg);
     }
     console.log(`Server starting on port ${appPort}`);
-    console.log('WHAT');
 });
 
 const shutdown = () => {

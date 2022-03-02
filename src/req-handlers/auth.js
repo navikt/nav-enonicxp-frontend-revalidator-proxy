5b9ef22508a06805b0e3e0c8aba51689d2e927ae
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-03-02 09:59:07 +0100

    Debug auth fail

--- src/req-handlers/auth.js
+++ src/req-handlers/auth.js
@@ -1,6 +1,8 @@
 const authHandler = (req, res, next) => {
     if (req.headers.secret !== process.env.SERVICE_SECRET) {
-        console.warn(`Auth failed on ${req.url}`);
+        console.warn(
+            `Auth failed on ${req.url} - ${req.headers.secret?.substr(0, 5)}`
+        );
         return res.status(401).send('Not authorized');
     }
 

c8b4f819ed4f8925d4e03eaa4e81cadee5eaca90
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-03-11 15:07:29 +0100

    Debug auth feil

--- src/req-handlers/auth.js
+++ src/req-handlers/auth.js
@@ -1,6 +1,11 @@
 const authMiddleware = (req, res, next) => {
     if (req.headers.secret !== process.env.SERVICE_SECRET) {
-        console.warn(`Auth failed on ${req.url}`);
+        console.warn(
+            `Auth failed on ${req.url} - ${req.headers.secret?.substr(
+                0,
+                4
+            )} - ${process.env.SERVICE_SECRET?.substr(0, 4)}`
+        );
         return res.status(401).send('Not authorized');
     }
 

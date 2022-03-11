526619adfc2a2b95694bbf8163916427fd2f48e5
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-03-11 16:13:05 +0100

    Fjerner debug-logging

--- src/req-handlers/auth.js
+++ src/req-handlers/auth.js
@@ -1,15 +1,6 @@
 const authMiddleware = (req, res, next) => {
     if (req.headers.secret !== process.env.SERVICE_SECRET) {
-        console.warn(
-            `Auth failed on ${req.url} - ${req.headers.secret?.substr(
-                5,
-                12
-            )} (${
-                req.headers.secret.length
-            }) - ${process.env.SERVICE_SECRET?.substr(5, 12)} (${
-                process.env.SERVICE_SECRET.length
-            })`
-        );
+        console.warn(`Auth failed on ${req.url}`);
         return res.status(401).send('Not authorized');
     }
 
